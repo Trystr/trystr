52965a6b51ae615652832c128c70139214047ea8
Thomas Dillard <thomas.dillard.studios@gmail.com>
2021-08-23 18:02:02 -0700

    Minor profile page fixes.

--- pages/profile.js
+++ pages/profile.js
@@ -10,10 +10,12 @@ export default function Profile() {
   const { user, isAuthenticated, isLoading } = useAuth0();
 
   if (!user) {
+    // TODO TD082321: redirect to login
     return <div></div>
   }
+
   return (
-    <Layout title="About">
+    <Layout title="Profile">
       <div className="max-w-3xl mx-auto divide-y-2 divide-gray-200">
         <dl className="mt-6 space-y-6 divide-y divide-gray-200">
           <img src={user.picture} alt={user.name} />
